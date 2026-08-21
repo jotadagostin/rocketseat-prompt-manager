76ce77e3ad2249b6cde1c5479e3d183f71336073
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-08-21 12:00:34 +0200

    settings configs

--- rocketseat-prompt-manager/src/app/page.tsx
+++ rocketseat-prompt-manager/src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h1>hello world!</h1>
-    </div>
+    <section className="flex min-h-full">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">Select a prompt</h1>
+          <p className="text-gray-400">
+            Choose a prompt of the list to view and edit
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
