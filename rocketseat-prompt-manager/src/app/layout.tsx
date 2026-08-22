e3549d3a80ad77a662efec802f0ef0a602ec9b3c
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-08-22 09:54:52 +0200

    setting button and sidebar

--- rocketseat-prompt-manager/src/app/layout.tsx
+++ rocketseat-prompt-manager/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Sidebar } from '@/components/sidebar/Sidebar';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
     <html lang="en">
       <body className={`${inter.variable} antialiased bg-gray-900 text-white`}>
         <section className="flex h-screen">
+          <Sidebar />
           <main className="relative flex-1 overflow-auto min-w-0">
             <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
               {children}
