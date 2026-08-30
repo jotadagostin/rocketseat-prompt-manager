fef39702496892fdec369d664dc7197554efe2e6
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-08-30 11:14:11 +0200

    setting interface
    PromptRepository and setting findMany and findSearch

---
+++ rocketseat-prompt-manager/src/core/aplication/prompts/search-prompts.use-case.ts
@@ -0,0 +1,14 @@
+import { PromptRepository } from '@/core/domain/prompts/prompt.respository';
+
+export class SearchPromptsUseCase {
+  constructor(private promptRepository: PromptRepository) {}
+  async execute(term: string) {
+    const q = term?.trim() ?? '';
+
+    if (!q) {
+      return this.promptRepository.findMany();
+    }
+
+    return this.promptRepository.searchMany(q);
+  }
+}
