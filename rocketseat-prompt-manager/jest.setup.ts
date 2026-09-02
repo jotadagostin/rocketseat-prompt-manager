1093a4220d20e600a41933eb62c8c0d4eed3245d
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2026-09-02 17:15:56 +0200

    Update jest.setup.ts

--- rocketseat-prompt-manager/jest.setup.ts
+++ rocketseat-prompt-manager/jest.setup.ts
@@ -1,3 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom';
+import { webcrypto } from 'crypto';
+import { TextDecoder, TextEncoder } from 'util';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any).TextEncoder = TextEncoder;
+(globalThis as any).TextDecoder = TextDecoder;
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
 
 expect.extend({});
